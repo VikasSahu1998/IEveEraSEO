ebee5f23480304daeeb62617c433e05dced17388
VikasSahu1998 <sahuvikas1998@gmail.com>
2023-04-13 23:39:38 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './Shared/footer/footer.component';
+import { HeaderComponent } from './Shared/header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
