5c1d4998f6e46e5e65407df23ff0ab51cd489b7a
VikasSahu1998 <sahuvikas1998@gmail.com>
2023-04-21 12:51:21 +0530

    header added

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
@@ -6,5 +10,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent {
+  ContantUsForm!: FormGroup | any;
 
-}
+  constructor(private formbuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.ContantUsForm = new FormGroup({
+      name: new FormControl('', [Validators.required,]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      PhoneNumber: new FormControl('', [Validators.required,]),
+      msg: new FormControl('', [Validators.required,]),
+    },);
+  }
+
+  Onsubmit() {
+    // if (this.ContantUsForm.valid)
+    //     .subscribe({
+    //       next: (res) => {
+    //        alert("data added");
+    //         this.ContantUsForm.reset();
+    //       },
+    //       error: () => {
+    //         alert("data error");
+    //       }
+    //     })
+
+    console.log('Your form data : ', this.ContantUsForm.value );
+      }
+  }
+
