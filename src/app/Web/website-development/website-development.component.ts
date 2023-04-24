e882139931cab7d425b86af26e1a23e6091ee40f
VikasSahu1998 <sahuvikas1998@gmail.com>
2023-04-24 12:07:56 +0530

    web  added

--- src/app/Web/website-development/website-development.component.ts
+++ src/app/Web/website-development/website-development.component.ts
@@ -6,5 +6,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./website-development.component.scss']
 })
 export class WebsiteDevelopmentComponent {
+  cards = [
+    {
+      Title: 'Web Portal Development',
+      Description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad blanditiislaboriosam odio magnam, recusandae nisi atque modi deserunt quas doloremque, eos animi debitisin rem eum, rerum eveniet officia soluta.',
+    },
+    {
+      Title: 'Custom Web Development',
+      Description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad blanditiis laboriosam odio magnam, recusandae nisi atque modi deserunt quas doloremque, eos animi debitisin rem eum, rerum eveniet officia soluta.',
+    },
+    {
+      Title: 'E-Commerce Web Development',
+      Description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad blanditiis laboriosam odio magnam, recusandae nisi atque modi deserunt quas doloremque, eos animi debitisin rem eum, rerum eveniet officia soluta.',
+    },
+    {
+      Title: 'CMS Web Development',
+      Description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad blanditiis laboriosam odio magnam, recusandae nisi atque modi deserunt quas doloremque, eos animi debitisin rem eum, rerum eveniet officia soluta.',
+    },
+    {
+      Title: 'Java/Php Web Development',
+      Description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad blanditiis laboriosam odio magnam, recusandae nisi atque modi deserunt quas doloremque, eos animi debitisin rem eum, rerum eveniet officia soluta.',
+    },
+    {
+      Title: 'Animation & Graphics Web Design',
+      Description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad blanditiis laboriosam odio magnam, recusandae nisi atque modi deserunt quas doloremque, eos animi debitisin rem eum, rerum eveniet officia soluta.',
+    }
+  ]
 
+  newcards = [
+    {
+      Title: 'One Stop Destination',
+      Description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad blanditiislaboriosam odio magnam, recusandae nisi atque modi deserunt quas doloremque, eos animi debitisin rem eum, rerum eveniet officia soluta.',
+    },
+    {
+      Title: 'Customer Satisfaction Is Our Prime Goal',
+      Description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad blanditiis laboriosam odio magnam, recusandae nisi atque modi deserunt quas doloremque, eos animi debitisin rem eum, rerum eveniet officia soluta.',
+    },
+    {
+      Title: 'We Love What We Do',
+      Description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad blanditiis laboriosam odio magnam, recusandae nisi atque modi deserunt quas doloremque, eos animi debitisin rem eum, rerum eveniet officia soluta.',
+    },
+    {
+      Title: 'Competitive Prices',
+      Description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad blanditiis laboriosam odio magnam, recusandae nisi atque modi deserunt quas doloremque, eos animi debitisin rem eum, rerum eveniet officia soluta.',
+    },
+    {
+      Title: 'Dedicated Professional Team',
+      Description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad blanditiis laboriosam odio magnam, recusandae nisi atque modi deserunt quas doloremque, eos animi debitisin rem eum, rerum eveniet officia soluta.',
+    },
+    {
+      Title: 'On-Time Delivery',
+      Description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad blanditiis laboriosam odio magnam, recusandae nisi atque modi deserunt quas doloremque, eos animi debitisin rem eum, rerum eveniet officia soluta.',
+    }
+  ]
 }
